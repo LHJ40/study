4384b24f92aa8bd2f43e2d08e03588ce31ca0218
LHJ <puppy970520@naver.com>
2024-01-13 22:22:39 +0900

    스프링

---
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,16 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+	private SingletonService() {
+		
+	}
+
+	private static final SingletonService instance= new SingletonService();
+
+	public static SingletonService getInstance() {
+		return instance;
+	}
+	
+	
+}
